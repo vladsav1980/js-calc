ca3f6e46797eb6080adc95ce0650d5c298fcea1e
vladsav1980 <vladsav1980@gmail.com>
2023-09-22 07:50:02 +0300

    js-calc

--- src/route/index.js
+++ src/route/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 // Підключіть файли роутів
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
+const calc = require('./calc')
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
 // Використовуйте інші файли роутів, якщо є
+router.use('/calc', calc)
 
 // Експортуємо глобальний роутер
 module.exports = router
